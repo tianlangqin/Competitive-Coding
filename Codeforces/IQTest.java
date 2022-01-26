160cc32dc5bf8c97ee8c7b63d78b1774a677f4c3
Q1N <68651371+Q1N122@users.noreply.github.com>
2022-01-26 00:23:21 -0500

    Add files via upload

---
+++ Codeforces/IQTest.java
@@ -0,0 +1,34 @@
+
+import java.util.Scanner;
+
+public class IQTest{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0 ; i < n ; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int even = 0 , odd = 0;
+        for(int i = 0 ; i < n ; i++) {
+            if(arr[i] % 2 == 0)
+                even++;
+            else
+                odd++;
+        }
+        if(even > odd) {
+            for(int i = 0 ; i < n ; i++) {
+                if(arr[i] % 2 != 0 ) {
+                    System.out.println(i + 1);
+                }
+            }
+        }
+        if(odd > even) {
+            for(int i = 0 ; i < n ; i++) {
+                if(arr[i] % 2 == 0) {
+                    System.out.println(i + 1);
+                }
+            }
+        }
+    }
+}
---
+++ Codeforces/OddDivisor.java
@@ -0,0 +1,26 @@
+
+import java.util.Scanner;
+
+public class OddDivisor {
+    public static boolean hasOddDivisor(long n ){
+        while(n > 1){
+            if(n % 2 == 0)
+                n /= 2;
+            else
+                return true;
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for(int i = 0 ; i < t ; i++){
+            long n = sc.nextLong();
+            if (hasOddDivisor(n ) == true )
+                System.out.println("YES");
+            else
+                System.out.println("NO");
+        }
+    }
+}
+
